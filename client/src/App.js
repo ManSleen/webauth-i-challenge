1149b8b52eec21cf68eea7f7435c233df945b5f7
Mike VanSleen <mike.vansleen@gmail.com>
2019-08-19 17:29:55 -0600

    Made a react app

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Login from './components/Login'
+import Register from './components/Register'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Heres the ol app!</h1>
+      <Login />
+      <Register />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Login.js
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+
+const Login = () => {
+
+    const [credentials, setCredentials] = useState({
+        username: "",
+        password: ""
+    })
+
+    const {username, password} = credentials;
+
+    const handleChanges = (e) => {
+        setCredentials({...credentials, [e.target.name] : e.target.value})
+        
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(credentials)
+    }
+
+    return(
+        <div>
+            <h2>Login</h2>
+            <form onSubmit={handleSubmit}>
+                <input placeholder="Username"type="text" value={username} name="username" onChange={handleChanges} />
+                <input placeholder="Password"type="password" value={password} name="password" onChange={handleChanges} />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    )
+}
+
+export default Login;
