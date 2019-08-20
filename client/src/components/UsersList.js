37e112beb20c89d6ab3d9074bbb4d46c506f4eeb
Mike VanSleen <mike.vansleen@gmail.com>
2019-08-20 17:59:28 -0600

    MVP complete

--- client/src/components/UsersList.js
+++ client/src/components/UsersList.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios'
 
-const UsersList = () => {
+const UsersList = ({getUsers}) => {
+
+  const [users, setUsers] = useState(null) 
+
+  
+  
+  useEffect(() => {
+    
+      axios.get("http://localhost:5001/api/users")
+        .then(res => {
+          setUsers(res)
+        })
+        .catch(err => {
+          console.log(err);
+        })
+    
+      
+  }, [])
+  
   return (
     <div>
       <h3>List of users!</h3>
+      {users ? users.map(user => <div>{user.username}</div>) : <h4>Loading...</h4>}
     </div>
   );
 };
